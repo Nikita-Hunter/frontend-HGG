4d2c0b3cca1bf0d95fc6fbee8a84484d0a325904
Nikita-Hunter <mochalov-hunter@mail.ru>
2021-11-03 21:20:48 +0300

    make carousel

--- src/js/global.js
+++ src/js/global.js
@@ -1,1 +1,26 @@
-console.log('global');
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 16,
+    items: 3,
+    nav: true,
+    center: true,
+    dots: false,
+    autoWidth:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    navText: ['<div class="owl-prev prevv"><span>previous</span></div>', '<div class="owl-next nextt"><span>next</span></div>'],
+    responsive : {
+      0 : {
+          items: 1
+      },
+      600 : {
+          items: 3
+      },
+      1200 : {
+        items: 3
+      },
+    },
+  });
+});
